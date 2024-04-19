b8f1bd265318dbe03921c631137409afd99a3042
Anna <35114047+AKoneva@users.noreply.github.com>
2024-04-19 10:41:54 +0900

    Init

---
+++ Gallery/Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Анна Перехрест  on 2024/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
